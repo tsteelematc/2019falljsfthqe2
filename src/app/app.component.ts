f6abac13f33dd0627f7a42f5171c2332a0707812
tsteelematc <tsteele@madisoncollege.edu>
2019-11-21 19:18:22 -0600

    Implemented edited quiz badges...
    We need stinken badges...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ interface QuizDisplay {
   questions: QuestionDisplay[];
   markedForDelete: boolean;
   newlyAddedQuiz: boolean;
+  naiveQuizChecksum: string;
 }
 
 interface QuestionDisplay {
@@ -59,12 +60,17 @@ export class AppComponent implements OnInit {
           name: x.name,
           questions: x.questions,
           markedForDelete: false,
-          newlyAddedQuiz: false
+          newlyAddedQuiz: false,
+          naiveQuizChecksum: this.generateNaiveQuizChecksum(x)
         }));
       }, error => {
         console.error(error.error);
         this.failedToLoadQuizzes = true;
       });
+  }
+
+  generateNaiveQuizChecksum(q: QuizDisplay) {
+    return q.name + q.questions.length + q.questions.map(x => '~' + x.name).join('');
   }
 
   selectQuiz(q) {
@@ -79,6 +85,7 @@ export class AppComponent implements OnInit {
       , questions: []
       , markedForDelete: false
       , newlyAddedQuiz: true
+      , naiveQuizChecksum: ""
     };
 
     this.quizzes = [
@@ -171,4 +178,15 @@ export class AppComponent implements OnInit {
     return this.quizzes.filter(x => x.newlyAddedQuiz && !x.markedForDelete);
   }
 
+  get numberOfEditedQuizzes() {
+    return this.getEditedQuizzes().length;
+  }
+
+  getEditedQuizzes() {
+    return this.quizzes
+      .filter(x => this.generateNaiveQuizChecksum(x) != x.naiveQuizChecksum 
+        && !x.newlyAddedQuiz 
+        && !x.markedForDelete
+      );
+  }
 }
